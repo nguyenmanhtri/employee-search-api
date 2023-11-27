759478e61d634d7fa1c4c6e0047f0888b00b945e
Tri Nguyen <tri.mn.nguyen@gmail.com>
2023-11-27 14:05:45 +0700

    more filters

--- search/views/employees.py
+++ search/views/employees.py
@@ -5,13 +5,14 @@ from rest_framework import viewsets
 from search.serializers.employees import EmployeeSerializer
 from search.models import Employees
 
+employee_fields = Employees._meta.get_fields()
+field_names = [f.name for f in employee_fields]
+
 
 class EmployeeFilterSet(django_filters.FilterSet):
-    id = django_filters.NumberFilter()
-
     class Meta:
         model = Employees
-        exclude = []
+        fields = {f: ["exact", "in", "icontains"] for f in field_names}
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
@@ -21,4 +22,4 @@ class EmployeeViewSet(viewsets.ModelViewSet):
 
     queryset = Employees.objects.all()
     serializer_class = EmployeeSerializer
-    filterset_fields = "__all__"
+    filterset_class = EmployeeFilterSet
